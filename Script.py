dd081f4de2f893cd14ab35ef923008bc8d7c6684
Nischal <nischalk789@gmail.com>
2025-02-06 04:57:45 +0530

    Commit 3

--- Script.py
+++ Script.py
@@ -6,5 +6,6 @@ int b = 20
 
 print("sum of two number : " , a + b)
 print("difference of two numbers : " , a - b)
-print("Multiplication of two numbers : " a * b )
-
+print("Multiplication of two numbers : ", a * b )
+print("Division of two numbers : ", a / b )
+print("remainder of two numbers : " , a % b)
