cd386f7ec9f778103ef34bdb46dcb34bc617bb20
Nischal <nischalk789@gmail.com>
2025-02-06 06:15:16 +0530

    Restored Script.py from commit b149629f

---
+++ Script.py
@@ -0,0 +1,10 @@
+
+print("Hello world")
+
+int a = 10
+int b = 20
+
+print("sum of two number : " , a + b)
+print("difference of two numbers : " , a - b)
+print("Multiplication of two numbers : " a * b )
+
